19109beecbbf176fdb672034f0491deb2838ff8f
David <ddaffdev@gmail.com>
2024-03-19 00:22:12 -0300

    Alfa Nvim

--- lua/daff/plugins/lsp/conform.lua
+++ lua/daff/plugins/lsp/conform.lua
@@ -4,7 +4,7 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			javascript = { "eslint_d" },
-			astro = { "prettier" },
+			astro = { "prettier", "eslint_d" },
 			typescript = { "eslint_d" },
 			python = { "black" },
 			css = { "prettier" },
--- lua/daff/plugins/nvim-cmp.lua
+++ lua/daff/plugins/nvim-cmp.lua
@@ -1,66 +1,67 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    "L3MON4D3/LuaSnip", -- snippet engine
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-    "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
-    "hrsh7th/cmp-nvim-lua",
-  },
-  config = function()
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- source for text in buffer
+		"hrsh7th/cmp-path", -- source for file system paths
+		"L3MON4D3/LuaSnip", -- snippet engine
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets", -- useful snippets
+		"onsails/lspkind.nvim", -- vs-code like pictograms
+		"hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+	},
+	config = function()
+		local cmp = require("cmp")
 
-    local luasnip= require("luasnip")
+		local luasnip = require("luasnip")
 
-    local lspkind = require ("lspkind")
+		local lspkind = require("lspkind")
 
-    -- Loads vscode style
-    require("luasnip.loaders.from_vscode").lazy_load()
+		-- Loads vscode style
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect"
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      -- Sources
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "nvim_lua" },
-      },
-      formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "..."
-        })
-      },
-      --mappings
-      mapping = cmp.mapping.preset.insert({
-      -- Confirm false
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			-- Sources
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+				{ name = "nvim_lua" },
+			},
+			formatting = {
+				format = lspkind.cmp_format({
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+			--mappings
+			mapping = cmp.mapping.preset.insert({
+				-- Confirm false
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-      -- Tab
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif require("luasnip").expand_or_jumpable() then
-            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-
-    })
-  end,
+				-- Tab
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						vim.fn.feedkeys(
+							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+							""
+						)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+		})
+	end,
 }
